3abae79e4f0668ba1e60b215efe43f1938f71a8f
Kevin Mui <kmui@sapir.psych.wisc.edu>
2020-05-19 17:27:23 -0500

    Allow index.html in url

--- utils/task-name.js
+++ utils/task-name.js
@@ -1,5 +1,5 @@
 // Task name is the last route in pathname
 export default window.location.pathname
   .split('/')
-  .filter((p) => p.length > 0)
+  .filter((p) => p.length > 0 && p !== 'index.html')
   .pop();
