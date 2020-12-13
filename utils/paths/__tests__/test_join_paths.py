3602f0851b84313f1e139a0510e1991af7e25671
Kevin Mui <kmui2@wisc.edu>
2020-12-13 15:20:28 -0600

    Remove unnecessary utils

--- utils/paths/__tests__/test_join_paths.py
+++
@@ -1,6 +0,0 @@
-from utils.paths.join_paths import join_paths
-
-
-def test_join_paths():
-    assert join_paths("/foo", "bar", "test") == "/foo/bar/test"
-    assert join_paths("/foo/", "/bar/", "/test/") == "/foo/bar/test"
--- utils/paths/__tests__/test_sanitize_path.py
+++
@@ -1,5 +0,0 @@
-from utils.paths.sanitize_path import sanitize_path
-
-
-def test_sanitize_path():
-    assert sanitize_path("/foo/../foo") == "/foo"
