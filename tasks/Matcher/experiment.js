0d42d135e1bf833f1067069f3030e4edd08e7164
Kevin Mui <kmui2@wisc.edu>
2021-03-27 01:07:05 -0500

    Add matcher task

---
+++ tasks/Matcher/experiment.js
@@ -0,0 +1,180 @@
+import createApi from '../../utils/create-api.js';
+import demographics_questions from './demograhpics.js';
+import searchParams from '../../utils/search-params.js';
+import PORT from './port.js';
+
+const errors = [];
+
+const old_console_error = console.error;
+console.error = function(message) {
+  errors.push(message);
+  old_console_error.apply(console, arguments);
+};
+
+const createErrorMessage = (error) =>
+  `<h3>Something went wrong. Please try reloading again and check your connection before contacting us.</h3>Unexpected error: ${
+    error.message
+  }.<br /> <br />Additional error logs:<br />${errors.join('<br />')}`;
+
+const handleError = (error) => {
+  console.error(error);
+  const error_message = createErrorMessage(error);
+  try {
+    jsPsych.endExperiment(error_message);
+  } catch (_) {
+    document.body.innerHTML = error_message;
+  }
+};
+
+(async () => {
+  try {
+    let { workerId: worker_id, fullscreen, reset } = searchParams;
+
+    // "task" will be automatically populated with the value of TASK
+    const api = createApi(PORT, handleError);
+
+    const {
+      trials,
+      num_trials,
+      consent_agreed,
+      completed_demographics,
+      ...trials_response
+    } = await api({
+      fn: 'trials',
+      kwargs: {
+        worker_id: worker_id || localStorage.getItem('workerId'),
+        reset,
+      },
+    });
+
+    const has_trials_remaining = trials.length > 0;
+
+    worker_id = trials_response['worker_id'];
+    localStorage.setItem('workerId', worker_id);
+
+    ////////////////////////////////
+    // Timeline
+    ////////////////////////////////
+    const main_timeline = [];
+
+    const fullscreen_trial = {
+      type: 'fullscreen',
+      fullscreen_mode: true,
+      message: '<p>This will switch to full screen mode when you press the button below</p>',
+      button_label: 'Continue',
+    };
+
+    if (fullscreen) main_timeline.push(fullscreen_trial);
+
+    const consent_trial = {
+      type: 'lupyanlab-consent',
+      url: './consent.html',
+      alert:
+        'If you wish to participate, you must check the box next to the statement "I agree to participate in this study."',
+      button_label: 'Start Experiment',
+      on_finish: () => {
+        return api({ fn: 'consent', kwargs: { worker_id } });
+      },
+    };
+
+    if (!consent_agreed) main_timeline.push(consent_trial);
+
+    const instructions = {
+      type: 'instructions',
+      pages: [/* html */ `<p class="lead">Placeholder for Instructions</p>`],
+      show_clickable_nav: true,
+    };
+    if (has_trials_remaining > 0) main_timeline.push(instructions);
+
+    const data_trials_block = {
+      type: 'lupyanlab-matcher',
+      timeline: trials.map((trial) => ({
+        prompt: `Clue: "${trial.clue}"\nclick on the correct words`,
+        trial_progress_text: `Trial ${trial.trial_num} of ${num_trials}`,
+        feedback_duration: 1000,
+        terms: [
+          [trial.target1, trial.target1_order],
+          [trial.target2, trial.target2_order],
+          [trial.target3, trial.target3_order],
+          [trial.distractor1, trial.distractor1_order],
+          [trial.distractor2, trial.distractor2_order],
+          [trial.distractor3, trial.distractor3_order],
+          [trial.distractor4, trial.distractor4_order],
+          [trial.distractor5, trial.distractor5_order],
+          [trial.distractor6, trial.distractor6_order],
+        ]
+          .sort((a, b) => a[1] - b[1])
+          .map((t) => t[0]),
+        on_start: () => jsPsych.setProgressBar((trial.trial_num - 1) / num_trials),
+        on_finish: ({ rt, response }) => {
+          return api({
+            fn: 'data',
+            kwargs: {
+              worker_id,
+              data: {
+                target1_response: response[trial.target1_order - 1],
+                target2_response: response[trial.target2_order - 1],
+                target3_response: response[trial.target3_order - 1],
+                distractor1_response: response[trial.distractor1_order - 1],
+                distractor2_response: response[trial.distractor2_order - 1],
+                distractor3_response: response[trial.distractor3_order - 1],
+                distractor4_response: response[trial.distractor4_order - 1],
+                distractor5_response: response[trial.distractor5_order - 1],
+                distractor6_response: response[trial.distractor6_order - 1],
+                rt,
+                ...trial,
+              },
+            },
+          });
+        },
+      })),
+    };
+    main_timeline.push(data_trials_block);
+
+    const demographics_questions_instructions = {
+      type: 'instructions',
+      pages: [
+        `<p class="lead">Thank you! We'll now ask a few demographic questions and you'll be transferred to a qualtrics survey. Then you'll be done!
+              </p>`,
+      ],
+      show_clickable_nav: true,
+    };
+    if (!completed_demographics) main_timeline.push(demographics_questions_instructions);
+
+    const demographics_trial = {
+      type: 'lupyanlab-surveyjs',
+      questions: demographics_questions,
+      on_finish: ({ response }) => {
+        return api({ fn: 'demographics', kwargs: { worker_id, demographics: response } });
+      },
+    };
+    if (!completed_demographics) main_timeline.push(demographics_trial);
+
+    const debrief_block = {
+      type: 'html-keyboard-response',
+      choices: [],
+      stimulus: function() {
+        return /* html */ `Thank you for participating!
+          <p>The purpose of this HIT is to assess the extent to which different people agree what makes
+          a particular dog, cat, or car typical.
+  
+          <p>
+          If you have any questions or comments, please email cschonberg@wisc.edu.`;
+      },
+    };
+    main_timeline.push(debrief_block);
+
+    jsPsych.init({
+      timeline: main_timeline,
+      fullscreen,
+      show_progress_bar: true,
+      auto_update_progress_bar: false,
+    });
+  } catch (error) {
+    console.error(error);
+    if (document.getElementById('preloader-gif-container') != null) {
+      document.getElementById('preloader-gif-container').remove();
+    }
+    document.body.innerHTML = createErrorMessage(error);
+  }
+})();
---
+++ tasks/Matcher/task.py
@@ -0,0 +1,166 @@
+from __future__ import annotations
+
+from os import mkdir
+from random import sample, shuffle
+from time import time
+from utils.csv.parse_str_list import parse_str_list
+
+from task_runner.app import app
+from utils.balance_utils.create_counts_file import create_counts_file
+from utils.balance_utils.get_next_min_key import get_next_min_key
+from utils.constants import ENV_FOLDER_PATH_KEY
+from utils.csv.append_to_csv import append_to_csv
+from utils.csv.get_remaining_trials import get_remaining_trials_with_trial_nums
+from utils.csv.read_rows import read_rows
+from utils.csv.write_to_csv import write_to_csv
+from utils.get_random_username import get_random_username
+from utils.listdir import listdir
+from utils.paths.create_join_paths_fn import create_join_paths_fn as create_safe_join_paths_fn
+from utils.paths.get_dirname import get_dirname
+from utils.remove_files import remove_files
+from utils.shuffle_without_catch_in_front import shuffle_without_catch_in_front
+
+dirname = get_dirname(__file__)
+
+TRIAL_NUM_COLUMN = "trial_num"
+NUM_LEADING_NON_CATCH_TRIALS = 2
+QUESTION_TYPE_COLUMN = "question_type"
+CATCH_VALUE = "catch"
+
+
+class Task:
+    def __init__(self, dev=False):
+        env_folder_path = dirname / ("dev" if dev else "prod")
+
+        # This is for different environments that can be set in Pytest
+        if ENV_FOLDER_PATH_KEY in app.config:
+            env_folder_path = app.config[ENV_FOLDER_PATH_KEY]
+
+        if not env_folder_path.exists():
+            mkdir(env_folder_path)
+
+        self.trial_lists_folder_path = dirname / "trial_lists"
+
+        # dev/prod folders/files
+        self.counts_file_path = env_folder_path / "counts.csv"
+
+        # dev/prod folder join paths functions for below to use
+        create_safe_join_paths_fn_env = lambda folder: create_safe_join_paths_fn(
+            env_folder_path / folder, mkdir=True
+        )
+        self.safe_join_paths_trials = create_safe_join_paths_fn_env("trials")
+        self.safe_join_paths_data = create_safe_join_paths_fn_env("data")
+        self.safe_join_paths_demographics = create_safe_join_paths_fn_env("demographics")
+        self.safe_join_paths_consent = create_safe_join_paths_fn_env("consent")
+
+    def trials(self, worker_id: str = None, reset: bool = False):
+        # If worker_id not provided, generate a random username with the seconds
+        # # since the epoch appended at the end.
+        if worker_id is None:
+            worker_id = f"{get_random_username()}_{int(time())}"
+        trials_file_path = self.safe_join_paths_trials(f"{worker_id}.csv")
+        demographics_file_path = self.safe_join_paths_demographics(f"{worker_id}.csv")
+        consent_file_path = self.safe_join_paths_consent(f"{worker_id}.txt")
+        data_file_path = self.safe_join_paths_data(f"{worker_id}.csv")
+
+        if reset or not trials_file_path.exists():
+            remove_files(
+                demographics_file_path, consent_file_path, data_file_path, trials_file_path
+            )
+            trials = self._generate_trials(worker_id)
+            num_trials = len(trials)
+
+            completed_demographics = False
+            consent_agreed = False
+        else:
+            trials, num_trials = get_remaining_trials_with_trial_nums(
+                trials_file_path,
+                data_file_path,
+                trial_num_column_name=TRIAL_NUM_COLUMN,
+            )
+
+            # Parse the CSV strings into their proper data type
+            for trial in trials:
+                trial[TRIAL_NUM_COLUMN] = int(trial[TRIAL_NUM_COLUMN])
+                trial["target1_order"] = int(trial["target1_order"])
+                trial["target2_order"] = int(trial["target2_order"])
+                trial["target3_order"] = int(trial["target3_order"])
+                trial["distractor1_order"] = int(trial["distractor1_order"])
+                trial["distractor2_order"] = int(trial["distractor2_order"])
+                trial["distractor3_order"] = int(trial["distractor3_order"])
+                trial["distractor4_order"] = int(trial["distractor4_order"])
+                trial["distractor5_order"] = int(trial["distractor5_order"])
+                trial["distractor6_order"] = int(trial["distractor6_order"])
+
+            completed_demographics = demographics_file_path.exists()
+            consent_agreed = consent_file_path.exists()
+
+        return {
+            "trials": trials,
+            "num_trials": num_trials,
+            "completed_demographics": completed_demographics,
+            "consent_agreed": consent_agreed,
+            "worker_id": worker_id,
+        }
+
+    def data(self, worker_id: str, data: dict):
+        data_file_path = self.safe_join_paths_data(f"{worker_id}.csv")
+        append_to_csv(data_file_path, data)
+
+    def demographics(self, worker_id: str, demographics: dict):
+        demographics_file_path = self.safe_join_paths_demographics(f"{worker_id}.csv")
+        write_to_csv(demographics_file_path, demographics)
+
+    def consent(self, worker_id: str):
+        """
+        Endpoint to mark consent as agreed.
+        """
+        consent_file_path = self.safe_join_paths_consent(f"{worker_id}.txt")
+        consent_file_path.touch()
+        consent_file_path.write_text("agreed")
+
+    ########################################################
+    # HELPERS
+    ########################################################
+    def _generate_trials(self, worker_id: str):
+        # Get assigned trial list
+        if not self.counts_file_path.exists():
+            trial_lists = [
+                trial_list_path.name for trial_list_path in listdir(self.trial_lists_folder_path)
+            ]
+            create_counts_file(self.counts_file_path, trial_lists)
+        trial_list = get_next_min_key(self.counts_file_path)
+
+        # Copy assigned trial list to trials folder
+        trial_list_path = self.trial_lists_folder_path / trial_list
+        trial_file_path = self.safe_join_paths_trials(f"{worker_id}.csv")
+        trials = read_rows(trial_list_path)
+        trials = shuffle_without_catch_in_front(
+            trials,
+            NUM_LEADING_NON_CATCH_TRIALS,
+            type_key=QUESTION_TYPE_COLUMN,
+            catch_type_value=CATCH_VALUE
+        )
+
+        order = list(range(1, 10))
+        shuffle(order)
+
+        # Add the trial_num column to the trials
+        trials = [
+            {
+                **row,
+                TRIAL_NUM_COLUMN: index + 1,
+                "target1_order": order[0],
+                "target2_order": order[1],
+                "target3_order": order[2],
+                "distractor1_order": order[3],
+                "distractor2_order": order[4],
+                "distractor3_order": order[5],
+                "distractor4_order": order[6],
+                "distractor5_order": order[7],
+                "distractor6_order": order[8],
+            } for index, row in enumerate(trials)
+        ]
+        write_to_csv(trial_file_path, trials)
+
+        return trials
